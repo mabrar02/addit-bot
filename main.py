81a87ba388fd52ca006a8c82413bac030fe049ce
mabrar02 <mahdeen02@gmail.com>
2023-08-10 01:23:53 -0300

    formatted summoner look up

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ async def getAcc(ctx, *, acc):
     api_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + acc
     api_req = api_url + "?api_key=" + API_KEY
     response = requests.get(api_req)
+    resp_json = response.json()
 
-    await ctx.send(response.json())
+
+    img_url = "http://ddragon.leagueoflegends.com/cdn/13.15.1/img/profileicon/%d.png" % (resp_json['profileIconId'])
+    acc_name = resp_json['name']
+    acc_level = resp_json['summonerLevel']
+
+    await ctx.send(img_url)
+    await ctx.send("Account Name: %s || Level: %d" % (acc_name, acc_level))
 
 client.run(AUTH_KEY)
